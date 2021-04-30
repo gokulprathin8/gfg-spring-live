1dd533e54f23564799a8116946e651960637a320
gokulprathin8 <gokulprathin8@gmail.com>
2021-05-01 05:28:03 +0530

    create nosql spring proj

---
+++ src/main/java/com/example/nosqlspringboot/NosqlSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.nosqlspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosqlSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NosqlSpringbootApplication.class, args);
+    }
+
+}
